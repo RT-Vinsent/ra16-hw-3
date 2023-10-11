502e93e7d9c7c0795be804307a3ac7c8736c4520
Tashtabanov Roman <RT.HAGBO@gmail.com>
2023-10-11 10:02:01 +0500

    hw-3, task-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import './App.css';
 import FilmsView from './components/task1/FilmsView/FilmsView';
+import LIstOffers from './components/task2/ListOffers/ListOffers';
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
           <h2>Задачи расположены последовательно в столбик</h2>
       </header>
       
+      {/* Компонент задачи №1 */}
       <FilmsView />
+
+      {/* Компонент задачи №2 */}
+      <LIstOffers />
       
       {/* Просто подвал */}
       <footer className='footer'><p>Просто подвал</p></footer>
